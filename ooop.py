89e6a9d4c4e4b0bd32443bc2636d2631f52737b1
eduper1 <eduper.havard@gmail.com>
2022-02-04 17:11:12 +0300

    static class

--- ooop.py
+++ ooop.py
@@ -1,54 +1,93 @@
+import csv
+
 # item1 = 'Phone'
 # item1_price = 100
 # item1_quantity = 5
 # item1_price_total = item1_price * item1_quantity
 # print(item1_price_total)
-# print(type(item1))  
+# print(type(item1))
+
 
 class Item:
-    pay_rate = 0.8 # the pay rate after20% discount
-    all = [] # Creating a list of all objects, each objects represents and instance
-    
-    def __init__(self, name: str, price: float, quantity=0):
+    pay_rate = 0.8  # the pay rate after20% discount
+    all = []  # Creating a list of all objects, each objects represents and instance
+
+    def __init__(self, name: str, price: float, quantity: 0):
         # Run validation on argument received
-        assert price >=0, f"Price {price} is not greater than or equal to zero" 
-        assert quantity >= 0, f"Quantity {quantity} is not greater  than or equal zero!"
-        
-        
+        #assert price >=0, f"Price {price} is not greater than or equal to zero"
+        # **** File "/home/sirus/Desktop/Python/oop/ooop.py", line 16, in __init_
+        #assert price >=0, f"Price {price} is not greater than or equal to zero"
+        # TypeError: '>=' not supported between instances of 'NoneType' and 'int
+        # Above error is same error type for quantity _
+        #assert quantity >= 0, f"Quantity {quantity} is not greater  than or equal zero!"
+
         # Assign to self object
         self.price = price
         self.quantity = quantity
         self.name = name
-        
+
         # Action to excute
         Item.all.append(self)
         # print(f"{name} object created ")
     # def calculate_total_price(self, x, y):
     #     return x * y
-    
+
     def calculate_total_price(self):
         return self.price * self.quantity
-    
+
     def apply_discount(self):
-        self.price = self.price * self.pay_rate # changed class attribute with self
+        self.price = self.price * self.pay_rate  # changed class attribute with self
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            print(item)
+            Item(
+                name=item.get('name'),
+                price=item.get('price'),
+                quantity=item.get('quantity'),
+            )
+            # Item(
+            #     name=item.get('name'),
+            #     price=float(item.get('price')),
+            #     quantity=int(item.get('quantity')),
+            # )
+    # represent all data object/instance in the list in a readable manner
+    @staticmethod
+    def is_integer(num): # static class is more regular function than a class function, it doesn't take self as 1st argument
+        # count out the floats that are point zero
+        # for i.e 5.0, 10.0
+        if isinstance(num, float):
+            # count out the floats that are point zero
+            return num.is_integer()
+    
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
         
+    
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-item1 = Item("Phone", 100, 5)
-item1.apply_discount()
-print(item1.price)
+# item1 = Item("Phone", 100, 5)
+# item1.apply_discount()
+# print(item1.price)
 # item1.name = "Phone"
 #item1.price = 100
 #item1.quantity = 5
-print(item1.calculate_total_price())
+# print(item1.calculate_total_price())
 
 # print(type(item1.name))
 
-item2 = Item("Laptop", 1000, 8)
-item2.pay_rate = 0.7 # change rate for different items set it self.pay_rate in the method
-item2.apply_discount()
-print(item2.price)
+# item2 = Item("Laptop", 1000, 8)
+# item2.pay_rate = 0.7 # change rate for different items set it self.pay_rate in the method
+# item2.apply_discount()
+# print(item2.price)
 # item2.name = "Laptop"
 #item2.price = 1000
 #item2.quantity = 3
@@ -66,10 +105,40 @@ print(item2.price)
 
 # print(Item.__dict__) # to know all class level attribute
 # print(item1.__dict__) # to know all instance level attribute
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+# item3 = Item("Cable", 10, 5)
+# item4 = Item("Mouse", 50, 5)
+# item5 = Item("Keyboard", 75, 5)
 
-print(Item.all)
-for instance in Item.all:
-    print(instance.name)
+# print(Item.all)
+# for instance in Item.all:
+#     print(instance.name)
+
+
+#Item.instantiate_from_csv()
+# print(Item.all)
+##****************** I GOT an ERROR SOLVE IT FIRST ##1:03
+
+
+# Accessing static method
+print(Item.is_integer(8.8))
+
+
+### 1: 09
+
+
+
+
+
+# class Intro:
+#     def __init__(self, name: str, gender: str, role: str, hobby: str):
+#         self.name = name
+#         self.gender = gender
+#         self.__role = role
+#         self.hobby = hobby
+
+#     def hi(self):
+#         return f'Hi! I am {self.name}, a {self.__role}.I speak: English, Swahili, Somali & Arabic.I like science, computer and {self.hobby}'
+
+
+# me = Intro('Mohamed', 'M', 'Programmer and ICT', 'robotics(Battlebot channel)')
+# print(me.hi())
